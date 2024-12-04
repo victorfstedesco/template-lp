0f1c4051e39c9272b0c2a7f03f8ba09bc1e2e858
victorfstedesco <victor.tedesco10@gmail.com>
2024-12-04 16:17:24 -0300

    template-pv

---
+++ public/data/index.js
@@ -0,0 +1,23 @@
+async function loadData() {
+    try {
+        
+        const response = await fetch('modulos.json')
+        const data = await response.json()
+
+        const containerhtml = document.getElementById('teste')
+        
+        data.forEach (datas => {
+            const div = document.createElement('div')
+            
+            div.innerHTML = `
+            <h1>${datas.tema}</h1>
+            `
+
+            containerhtml.appendChild(div)
+        })
+    } catch (erro) {
+        console.error('Erro ao carregar os dados ', erro)
+} 
+} 
+ 
+loadData()
